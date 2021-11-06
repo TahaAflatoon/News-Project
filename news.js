dc4ae5d36389c1e2c9f6c4f0f5175fbd48f05a70
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-11-06 16:03:39 +0330

    improved syntax & readability

--- news.js
+++ news.js
@@ -1,27 +1,26 @@
-/*JQuery*/
+// JQuery
 $.noConflict()
 jQuery(document).ready(function($) {
-    //mobile dropdown
+    // Mobile dropdown
     $('.mobile-dropdown').click(function() {
         $('.mobile-dropdown-content').slideToggle(500)
     }),
-    //close button
+    // Close button
     $('.closebtn').click(function() {
         $('.alert').css('display', 'none');
     })
 })
 
-/*JavaScript*/
-//Scroll indicator
-//when the user scrolls the page, execute myFunction
+// JavaScript
+// Scroll indicator
+// When the user scrolls the page, execute myFunction
 function indicator() {
     window.onscroll = function() {ScrollFunction()};
     function ScrollFunction() {
-        let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        let scrolled = (winScroll / height) * 100;
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrolled = (winScroll / height) * 100;
         document.getElementById("ScrollBar").style.width = scrolled + "%";
         }
 }
 indicator()
-/*-----------*/
